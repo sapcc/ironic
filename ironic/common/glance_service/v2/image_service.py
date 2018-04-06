3ac5798b2734eda04f58dbdd70493b7036564a93
Fabian Wiesel <fabian.wiesel@sap.com>
2018-04-06 13:32:21 +0200

    Fix glance multi-tenant logic

--- ironic/common/glance_service/v2/image_service.py
+++ ironic/common/glance_service/v2/image_service.py
@@ -145,17 +145,8 @@ class GlanceImageService(base_image_service.BaseImageService,
 
         if CONF.glance.swift_store_multi_tenant:
             LOG.debug("Getting temp-url for multi-tenant setup")
-            direct_url = image_info['properties'].get('direct_url')
-
-            chunks = urlparse.urlsplit(direct_url) if direct_url else None
-            container_id = object_id = None
-            if chunks and chunks.path:
-                parts = chunks.path.strip('/').split('/')
-                if len(parts) == 2:
-                    container_id, object_id = parts
-            else:
-                container_id = "glance_%s" % image_id
-                object_id = image_id
+            container_id = "glance_%s" % image_id
+            object_id = image_id
 
             swift = SwiftAPI(container_project_id=image_info['owner'])
             return swift.get_temp_url(container=container_id, object=object_id,
