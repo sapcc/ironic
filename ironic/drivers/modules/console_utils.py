5f6644a030cf14bbf11dbf56650a01aee96607fd
Hipfel, Stefan <stefan.hipfel@sap.com>
2020-07-06 16:50:27 +0200

    fixes console_url hash string encoding

--- ironic/drivers/modules/console_utils.py
+++ ironic/drivers/modules/console_utils.py
@@ -174,7 +174,7 @@ def get_shellinabox_console_url(port, uuid=None):
                 'uuid': uuid,
                 'expiry': expiry,
                 'secret': CONF.console.url_auth_digest_secret }
-            h.update(to_sign)
+            h.update(to_sign.encode('utf-8'))
             if digest_algorithm == 'base64':
                 digest = urlsafe_b64encode(h.digest())
             else:
